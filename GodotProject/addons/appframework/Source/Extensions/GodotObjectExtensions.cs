01c9e1cf6aae8c221a86fca0ac100fd12c9c4348
WiggleWizard <1405402+WiggleWizard@users.noreply.github.com>
2024-04-19 15:08:41 +0100

    Patched up the GodotObject extension code

--- GodotProject/addons/appframework/Source/Extensions/GodotObjectExtensions.cs
+++ GodotProject/addons/appframework/Source/Extensions/GodotObjectExtensions.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection;
 using Godot;
+
+using System.Reflection;
 
 namespace GodotAppFramework.Extensions;
 
 public static class GodotObjectExtensions
 {
-    /// <summary>
-    /// Note: Do not use this in hot path.
-    /// </summary>
-    /// <param name="godotObject"></param>
-    /// <param name="exportName"></param>
-    /// <returns></returns>
-    public static bool EnsureExportValid(this GodotObject thisObject, string exportName, bool mandatory = true)
+    public static bool IsExportValid(this GodotObject thisObject, string exportName)
     {
         var thisType = thisObject.GetType();
         var prop = thisType.GetProperty(exportName);
         if (prop == null)
         {
-            GD.PushError($"Property {exportName} does not exist for {thisObject.GetType().Name}");
             return false;
         }
 
-        var value = prop.GetValue(exportName, null);
+        return thisObject.IsExportValid(prop);
+    }
+
+    public static bool IsExportValid(this GodotObject thisObject, PropertyInfo prop)
+    {
+        var value = prop.GetValue(thisObject, null);
         if (value is GodotObject godotObject)
         {
             if (GodotObject.IsInstanceValid(godotObject))
@@ -32,14 +29,6 @@ public static class GodotObjectExtensions
             }
         }
 
-        if (mandatory)
-        {
-            GD.PushError($"Mandatory export of {exportName} in {thisType.Name} is not assigned with a valid node");
-            throw new Exception();
-        }
-        
-        GD.PushWarning($"Export of {exportName} in {thisType.Name} is not assigned with a valid node");
-
-        return false;
+        return false;   
     }
 }
